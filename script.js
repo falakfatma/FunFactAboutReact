403697319e07afaf44546b773dace0a7bea2daca
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-07-16 11:58:45 +0000

    added html JSX .

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>hello world</h1>,document.getElementById("root"))
+// // // ReactDOM.render(document.getElementById('root'))
+// // let h1 = document.createElement("h1")
+// // h1.textContent="hello"
+// // h1.className="header"
+// // document.getElementById("root").appendChild(h1)
+let navbar = (
+  <nav>
+  <h1>Brny</h1>
+  <ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contant</li>
+    </ul>
+  </nav>)
+ document.getElementById("root").append(JSON.stringify(navbar))
+// ReactDOM.render(navbar,document.getElementById('root'))
+
